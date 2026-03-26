53ce13a5dc50d4d398fcd2444c697e8d3fd7cac7
jimwu0614 <103909171+jimwu0614@users.noreply.github.com>
2026-03-26 15:22:56 +0800

    first commit

---
+++ backend/card.php
@@ -0,0 +1,33 @@
+<?php
+
+
+
+    class Card {
+        private $suit; // 花色: Spades, Hearts, Diamonds, Clubs
+        private $rank; // 點數: 2-10, J, Q, K, A
+
+        public function __construct($suit, $rank) {
+            $this->suit = $suit;
+            $this->rank = $rank;
+        }
+
+        public function getSuit() {
+            return $this->suit;
+        }
+
+        public function getRank() {
+            return $this->rank;
+        }
+
+        // 方便印出卡牌的文字 (例如: ♠A, ♥10)
+        public function toString() {
+            $symbols = [
+                'Spades' => '♠',
+                'Hearts' => '♥',
+                'Diamonds' => '♦',
+                'Clubs' => '♣'
+            ];
+            return $symbols[$this->suit] . $this->rank;
+        }
+    }
+?>
